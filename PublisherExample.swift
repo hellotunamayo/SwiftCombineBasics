501f9b88d10d601893123df6735e19eabddbc82b
Min Yoo <artfrige@gmail.com>
2024-06-08 19:39:18 +0900

    Create PublisherExample.swift

---
+++ PublisherExample.swift
@@ -0,0 +1,14 @@
+import Foundation
+import Combine
+
+//publisher
+let publisher = [0,1,2,3,4,5].publisher
+
+//subscriber receives value that emitted from publisher and prints even numbers.
+let subscriber = publisher
+    .filter {
+        $0 % 2 == 0 //filtering even number from received value
+    }
+    .sink { intValue in
+        print("\(intValue)")
+    }

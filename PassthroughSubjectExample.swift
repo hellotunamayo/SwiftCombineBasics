bf3abab40d7616026794992da4d0e18edcf051d3
Min Yoo <artfrige@gmail.com>
2024-06-08 20:06:58 +0900

    Update PassthroughSubjectExample.swift

--- PassthroughSubjectExample.swift
+++ PassthroughSubjectExample.swift
@@ -1,6 +1,12 @@
 import Foundation
 import Combine
 
+/*
+    PassthroughSubject
+    1. Doesn't have initial value
+    2. Subscriber can receive values from the publisher starting from the point they subscribe
+*/
+
 //First generic type is type be published.
 //Second one is error type.
 //If error is unnecesary, just put Never on it.

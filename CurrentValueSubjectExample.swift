7bb03bec04acaab6a8f4e7c5b8197c5e13782b1a
Min Yoo <yoo@minyoungyoo.com>
2024-06-17 17:00:35 +0900

    Update CurrentValueSubjectExample.swift

--- CurrentValueSubjectExample.swift
+++ CurrentValueSubjectExample.swift
@@ -11,15 +11,15 @@ import Combine
 let publisher = CurrentValueSubject<Int, Never>(100) //100 is initial value
 
 //First Subscriber
-let subscriber1: AnyCancellable = publisher.sink { integer in
+let subscription1: AnyCancellable = publisher.sink { integer in
     print("integer1 is: \(integer)") //The initial value 100 is print once at this point
 }
 
-publisher.send(200) //Subscriber1 prints 200 the updated value 200
+publisher.send(200) //Subscription1 prints 200 the updated value 200
 
 //Second Subscriber
-let subscriber2 = publisher.sink { integer in
-    print("integer2 is: \(integer)") //Subscriber2 printss 200 the initial value at subscription point
+let subscription2 = publisher.sink { integer in
+    print("integer2 is: \(integer)") //Subscription2 printss 200 the initial value at subscription point
 }
 
 publisher.send(300) //All Subscribers print 300
